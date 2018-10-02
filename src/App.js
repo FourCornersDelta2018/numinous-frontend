1e50f6f2f90d03233b24e50f7b04f62e470e3457
Sarah Proctor <sproctor950@gmail.com>
2018-10-02 16:11:37 -0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Footer from './components/footer'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <Footer />
       </div>
     );
   }
---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+      <img className="logo" src="/assets/logo.jpg" alt="logo"/> <br />
+        <p id="copyright"> copyright ©2018 Four_Corners </p>
+
+      </div>
+    );
+  }
+}
+
+export default Footer;
