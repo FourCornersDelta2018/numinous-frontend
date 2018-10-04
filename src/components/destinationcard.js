5d3ce2d80a4c734c207195e323180928827e9678
Sarah Proctor <sproctor950@gmail.com>
2018-10-04 14:49:52 -0700

    DestinationCard update

--- src/components/destinationcard.js
+++ src/components/destinationcard.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class DestinationCard extends Component {
     render() {
         let {img_path} = this.props.destination
+
         return(
             <div>
                 <img src ={img_path} alt ="picture of destination" />
