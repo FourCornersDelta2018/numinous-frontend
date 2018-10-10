ebbe615471830fb4dce32e7c0bafbdb91a3bc49d
Kevin Kochanski <kevinandrew6@Kevins-MBP-4.localdomain>
2018-10-10 10:44:02 -0700

    About us lame version

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,49 @@ import React, { Component } from 'react'
 class About extends Component {
   render() {
       return (
-          <div className="about"> WOOOOOOO!!!
+        <div>
+          <div className="box1">
+             <div className="column01">
+               <img src="/assets/teamphotos/FourCorners.jpg" alt="logo"/>
+               <h3>Four Corners</h3>
+               <p>From the four corners of the United States, Four Corners: Queer Eye for the Straight Girl is comprised of four lovely Delta Unicorns, all (soon-to-be) graduates of the LEARN Academy in San Diego, CA.</p>
+             </div>
           </div>
+          <div className="box1">
+             <div className="column02">
+               <h3>Caitlin Johnson</h3>
+               <p>Genius from Georgia. From the four corners of the United States, Four Corners: Queer Eye for the Straight Girl is comprised of four Delta Unicorns, all graduates of the LEARN Academy in San Diego, CA.</p>
+               <img src="/assets/teamphotos/CaitlinJohnson.jpg" alt="Headshot woman"/>
+             </div>
+          </div>
+          <div className="box1">
+             <div className="column01">
+               <img src="/assets/teamphotos/SarahProctor.jpg" alt="Headshot woman"/>
+               <h3>Sarah Proctor</h3>
+               <p>Awesomeness from Idaho. From the four corners of the United States, Four Corn=ers: Queer Eye for the Straight Girl is comprised of four Delta Unicorns, all graduates of the LEARN Academy in San Diego, CA.</p>
+             </div>
+          </div>
+          <div className="box1">
+             <div className="column02">
+               <h3>Tricia Sykes</h3>
+               <p>Superstar from SoCal. From the four corners of the United States, Four Corners: Queer Eye for the Straight Girl is comprised of four Delta Unicorns, all graduates of the LEARN Academy in San Diego, CA.</p>
+               <img src="/assets/teamphotos/TriciaSykes.jpg" alt="Headshot woman"/>
+             </div>
+          </div>
+          <div className="box1">
+             <div className="column01">
+               <img src="/assets/teamphotos/KevinKochanski.jpg" alt="Headshot man"/>
+               <h3>Kevin Kochanski</h3>
+               <p>Mincing Mary from Maryland. From the four corners of the United States, Four Corners: Queer Eye for the Straight Girl is comprised of four Delta Unicorns, all graduates of the LEARN Academy in San Diego, CA.</p>
+             </div>
+          </div>
+
+          <br />
+                    <br />
+                              <br />
+                                        <br />
+                                                  <br />
+        </div>
       )
   }
 }
