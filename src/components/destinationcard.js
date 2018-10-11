8b64bec71867bac24997452478ec8da80b5d8a35
Caitlin Johnson <caitlin.ch.johnson@gmail.com>
2018-10-11 14:36:27 -0700

    Added filtering functionality

--- src/components/destinationcard.js
+++ src/components/destinationcard.js
@@ -4,7 +4,6 @@ class DestinationCard extends Component {
     render() {
         const { destination } = this.props
         const { dest_name, img_path } = destination
-
         return (
             <div style={{padding: "1rem"}}>
                 <img className="card"src={img_path} alt={dest_name} />
