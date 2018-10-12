d4be612f7456b7e2cb6968ef0c8fa30e1ad9868b
Caitlin Johnson <caitlin.ch.johnson@gmail.com>
2018-10-12 14:03:53 -0700

    fixed the aws img path...

--- src/components/destinationcard.js
+++ src/components/destinationcard.js
@@ -5,7 +5,7 @@ class DestinationCard extends Component {
       console.log(this.props);
         const { destination } = this.props
         const { dest_name, img_path } = destination
-        let aws_img_path = `https://s3-us-west-2.amazonaws.com/numinous.images/images${img_path}`
+        let aws_img_path = `https://s3-us-west-2.amazonaws.com/numinous.images${img_path}`
         return (
             <div style={{padding: "1rem"}}>
                 <img className="card"src={aws_img_path} alt={dest_name} />
